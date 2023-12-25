cd491118835341b576f1aed81823c4ed70f3bf17
Mridul24agaga <101731772+Mridul24agaga@users.noreply.github.com>
2023-12-25 14:13:53 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,19 +16,16 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
-            </span>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-red-800">
+  We are
+</span>
+
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Web developers",
                 1000,
-                "Web Developer",
-                1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
+                "Mobile developers",
                 1000,
               ]}
               wrapper="span"
@@ -37,24 +34,17 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+          We are Your Top-Rated Web Development Agency! We take pride in crafting exceptional digital experiences. At SiteCraftDynamics, we blend creativity and expertise to deliver cutting-edge web solutions tailored to your needs. Whether it's building eye-catching websites, developing robust web applications, or optimizing your online presence, we've got you covered.
           </p>
           <div>
-            <Link
-              href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
-            >
-              Hire Me
-            </Link>
-            <Link
-              href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-            >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
-              </span>
-            </Link>
+          <Link
+  href="/#contact"
+  className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-gray-200 text-black"
+>
+  Lets Connect
+</Link>
+
+            
           </div>
         </motion.div>
         <motion.div
